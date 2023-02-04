b3e5863f68136673c9a77e28c45a941055dc5617
nimikevin <97411659+nimikevin@users.noreply.github.com>
2023-02-04 04:31:09 -0500

    controller:restruturation du code

--- controllers/task.controller.js
+++ controllers/task.controller.js
@@ -1,79 +1,98 @@
 const TaskModel = require("../models/task.model");
+const mongoose = require("mongoose");
 
-//get All task from mongoDb
-module.exports.getTasks = async (req, res) => {
-  try {
-    const tasks = await TaskModel.find();
-    if (!tasks.length) {
-      res.status(400).json({ message: "no task in BD" });
-    } else {
-      res.status(200).json(tasks);
+module.exports = {
+  //get All task from mongoDb
+  async getTasks(req, res) {
+    try {
+      const tasks = await TaskModel.find();
+      if (!tasks.length) {
+        res.status(400).json({ message: "no task in BD" });
+      } else {
+        res.status(200).json(tasks);
+      }
+    } catch (error) {
+      res.status(500).json({ message: error.message });
     }
-  } catch (error) {
-    res.status(500).json({ message: error.message });
-  }
-};
+  },
 
-//get task by id
-module.exports.getTasksById = async (req, res) => {
-  try {
-    const tasks = await TaskModel.findById(req.params.id);
-    if (!tasks) {
-      res.status(400).json({ message: "task does not exist witch id" });
-    } else {
-      res.status(200).json(tasks);
+  //get task by id
+  async getTasksById(req, res) {
+    try {
+      if (!mongoose.isValidObjectId(req.params.id)) {
+        res.status(400).json({ message: "Invalid Task ID" });
+      } else {
+        const tasks = await TaskModel.findById(req.params.id);
+        if (!tasks) {
+          res.status(400).json({ message: "task does not exist witch id" });
+        } else {
+          res.status(200).json(tasks);
+        }
+      }
+    } catch (error) {
+      res.status(500).json({ message: error.message });
     }
-  } catch (error) {
-    res.status(500).json({ message: error.message });
-  }
-};
+  },
 
-//create task
-module.exports.setTasks = async (req, res) => {
-  try {
-    const { name } = req.body;
-    if (!name) {
-      res.status(400).json({ message: "please add a task" });
-    } else {
-      const tasks = await TaskModel.create({
-        name: req.body.name,
-      });
-      res.status(200).json(tasks);
+  //create task
+  async setTasks(req, res) {
+    try {
+      const { name } = req.body;
+      if (!name) {
+        res.status(400).json({ message: "please add a task" });
+      } else {
+        const tasks = await TaskModel.create({
+          name: req.body.name,
+        });
+        res.status(200).json(tasks);
+      }
+    } catch (error) {
+      res.status(500).json({ message: error.message });
     }
-  } catch (error) {
-    res.status(500).json({ message: error.message });
-  }
-};
+  },
 
-//delete task from mongodb
-module.exports.deleteTasks = async (req, res) => {
-  try {
-    const tasks = await TaskModel.findById(req.params.id);
-    if (!tasks) {
-      res.status(400).json({ message: "task does not exist witch id" });
-    } else {
-      await tasks.remove();
-      res.status(200).json("task deleted " + req.params.id);
+  //delete task from mongodb
+  async deleteTasks(req, res) {
+    try {
+      if (!mongoose.isValidObjectId(req.params.id)) {
+        res.status(400).json({ message: "Invalid Task ID" });
+      } else {
+        const tasks = await TaskModel.findById(req.params.id);
+        if (!tasks) {
+          res.status(400).json({ message: "task does not exist witch id" });
+        } else {
+          await tasks.remove();
+          res.status(200).json("task deleted " + req.params.id);
+        }
+      }
+    } catch (error) {
+      res.status(500).json({ message: error.message });
     }
-  } catch (error) {
-    res.status(500).json({ message: error.message });
-  }
-};
+  },
 
-//update task
-module.exports.updateTasksById = async (req, res) => {
-  try {
-    const tasks = await TaskModel.findById(req.params.id);
-    if (!tasks) {
-      res.status(400).json({ message: "task does not exist witch id" });
-    } else {
-      const updatePost = await TaskModel.findByIdAndUpdate(tasks, req.body, {
-        new: true,
-        runValidators: true,
-      });
-      res.status(200).json(updatePost);
+  //update task
+  async updateTasksById(req, res) {
+    try {
+      if (!mongoose.isValidObjectId(req.params.id)) {
+        res.status(400).json({ message: "Invalid Task ID" });
+      } else {
+        const tasks = await TaskModel.findById(req.params.id);
+        if (!tasks) {
+          res.status(400).json({ message: "task does not exist witch id" });
+        } else {
+          const updatePost = await TaskModel.findByIdAndUpdate(
+            tasks,
+            req.body,
+            {
+              new: true,
+              runValidators: true,
+            }
+          );
+          res.status(200).json(updatePost);
+        }
+      }
+    } catch (error) {
+      res.status(500).json({ message: error.message });
     }
-  } catch (error) {
-    res.status(500).json({ message: error.message });
-  }
+  },
 };
