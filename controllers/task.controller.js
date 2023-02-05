64c4c38a1e6741d5086026f7a529704605a0b5d3
nimikevin <97411659+nimikevin@users.noreply.github.com>
2023-02-05 04:34:41 -0500

    controller:introduction du Early Return Pattern

--- controllers/task.controller.js
+++ controllers/task.controller.js
@@ -7,10 +7,9 @@ module.exports = {
     try {
       const tasks = await TaskModel.find();
       if (!tasks.length) {
-        res.status(400).json({ message: "no task in BD" });
-      } else {
-        res.status(200).json(tasks);
+        return res.status(400).json({ message: "no task in BD" });
       }
+      return res.status(200).json(tasks);
     } catch (error) {
       res.status(500).json({ message: error.message });
     }
@@ -20,15 +19,16 @@ module.exports = {
   async getTasksById(req, res) {
     try {
       if (!mongoose.isValidObjectId(req.params.id)) {
-        res.status(400).json({ message: "Invalid Task ID" });
-      } else {
-        const tasks = await TaskModel.findById(req.params.id);
-        if (!tasks) {
-          res.status(400).json({ message: "task does not exist witch id" });
-        } else {
-          res.status(200).json(tasks);
-        }
+        return res.status(400).json({ message: "Invalid Task ID" });
       }
+      const tasks = await TaskModel.findById(req.params.id);
+
+      if (!tasks) {
+        return res
+          .status(400)
+          .json({ message: "task does not exist witch id" });
+      }
+      return res.status(200).json(tasks);
     } catch (error) {
       res.status(500).json({ message: error.message });
     }
@@ -38,14 +38,16 @@ module.exports = {
   async setTasks(req, res) {
     try {
       const { name } = req.body;
+
       if (!name) {
-        res.status(400).json({ message: "please add a task" });
-      } else {
-        const tasks = await TaskModel.create({
-          name: req.body.name,
-        });
-        res.status(200).json(tasks);
+        return res.status(400).json({ message: "please add a task" });
       }
+
+      const tasks = await TaskModel.create({
+        name,
+      });
+
+      return res.status(200).json(tasks);
     } catch (error) {
       res.status(500).json({ message: error.message });
     }
@@ -56,15 +58,17 @@ module.exports = {
     try {
       if (!mongoose.isValidObjectId(req.params.id)) {
         res.status(400).json({ message: "Invalid Task ID" });
-      } else {
-        const tasks = await TaskModel.findById(req.params.id);
-        if (!tasks) {
-          res.status(400).json({ message: "task does not exist witch id" });
-        } else {
-          await tasks.remove();
-          res.status(200).json("task deleted " + req.params.id);
-        }
       }
+
+      const tasks = await TaskModel.findById(req.params.id);
+
+      if (!tasks) {
+        return res
+          .status(400)
+          .json({ message: "task does not exist witch id" });
+      }
+      await tasks.remove();
+      return res.status(200).json("task deleted " + req.params.id);
     } catch (error) {
       res.status(500).json({ message: error.message });
     }
@@ -74,23 +78,21 @@ module.exports = {
   async updateTasksById(req, res) {
     try {
       if (!mongoose.isValidObjectId(req.params.id)) {
-        res.status(400).json({ message: "Invalid Task ID" });
-      } else {
-        const tasks = await TaskModel.findById(req.params.id);
-        if (!tasks) {
-          res.status(400).json({ message: "task does not exist witch id" });
-        } else {
-          const updatePost = await TaskModel.findByIdAndUpdate(
-            tasks,
-            req.body,
-            {
-              new: true,
-              runValidators: true,
-            }
-          );
-          res.status(200).json(updatePost);
-        }
+        return res.status(400).json({ message: "Invalid Task ID" });
       }
+      const tasks = await TaskModel.findById(req.params.id);
+
+      if (!tasks) {
+        return res
+          .status(400)
+          .json({ message: "task does not exist witch id" });
+      }
+
+      const updateTask = await TaskModel.findByIdAndUpdate(tasks, req.body, {
+        new: true,
+        runValidators: true,
+      });
+      return res.status(200).json(updateTask);
     } catch (error) {
       res.status(500).json({ message: error.message });
     }
